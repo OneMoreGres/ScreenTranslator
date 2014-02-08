1081379fb06d5a735df361adf027183e9178689b
Gres <gres@gres.biz>
2014-02-08 19:01:21 +0400

    Translation file added

---
+++ translations/translation_en.ts
@@ -0,0 +1,220 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>Manager</name>
+    <message>
+        <location filename="../Manager.cpp" line="80"/>
+        <source>Захват</source>
+        <translation>Capture</translation>
+    </message>
+    <message>
+        <location filename="../Manager.cpp" line="81"/>
+        <location filename="../Manager.cpp" line="190"/>
+        <location filename="../Manager.cpp" line="207"/>
+        <source>Перевод</source>
+        <translation>Translation</translation>
+    </message>
+    <message>
+        <location filename="../Manager.cpp" line="82"/>
+        <source>Повторить</source>
+        <translation>Repeat</translation>
+    </message>
+    <message>
+        <location filename="../Manager.cpp" line="84"/>
+        <source>Скопировать</source>
+        <translation>Copy</translation>
+    </message>
+    <message>
+        <location filename="../Manager.cpp" line="86"/>
+        <source>Настройки</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="../Manager.cpp" line="87"/>
+        <location filename="../Manager.cpp" line="157"/>
+        <source>О программе</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <location filename="../Manager.cpp" line="88"/>
+        <source>Выход</source>
+        <translation>Exit</translation>
+    </message>
+    <message>
+        <location filename="../Manager.cpp" line="153"/>
+        <source>Программа для распознавания текста на экране.
+Создана с использованием Qt, tesseract-ocr, Google Translate.
+Автор: Gres (translator@gres.biz)</source>
+        <translation>Screen text recognition and translation program. \n Uses Qt,  tesseract-ocr, Google Translate. \n Author: Gres (translator@gres.biz)</translation>
+    </message>
+    <message>
+        <location filename="../Manager.cpp" line="191"/>
+        <source>Последний перевод был скопирован в буфер обмена.</source>
+        <translation>Last translation has been copied to the clipboard.</translation>
+    </message>
+    <message>
+        <location filename="../Manager.cpp" line="214"/>
+        <source>Ошибка</source>
+        <translation>Error</translation>
+    </message>
+</context>
+<context>
+    <name>Recognizer</name>
+    <message>
+        <location filename="../Recognizer.cpp" line="40"/>
+        <source>Неверные параметры для OCR</source>
+        <translation>Wrong parameters for OCR</translation>
+    </message>
+    <message>
+        <location filename="../Recognizer.cpp" line="52"/>
+        <source>Ошибка инициализации OCR: %1</source>
+        <translation>OCR initialization error: %1</translation>
+    </message>
+    <message>
+        <location filename="../Recognizer.cpp" line="94"/>
+        <source>Текст не распознан.</source>
+        <translation>Failed to recognize text.</translation>
+    </message>
+</context>
+<context>
+    <name>ResultDialog</name>
+    <message>
+        <location filename="../ResultDialog.ui" line="14"/>
+        <source>Результат</source>
+        <translation>Result</translation>
+    </message>
+</context>
+<context>
+    <name>SelectionDialog</name>
+    <message>
+        <location filename="../SelectionDialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialog</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsEditor</name>
+    <message>
+        <location filename="../SettingsEditor.ui" line="14"/>
+        <source>Настройки</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="../SettingsEditor.ui" line="20"/>
+        <source>Горячие клавиши</source>
+        <translation>Hotkeys</translation>
+    </message>
+    <message>
+        <location filename="../SettingsEditor.ui" line="26"/>
+        <location filename="../SettingsEditor.ui" line="42"/>
+        <location filename="../SettingsEditor.ui" line="58"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Сочетание клавиш для перехода в режим захвата.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Keys combination to enter capture mode.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../SettingsEditor.ui" line="29"/>
+        <source>Захватить</source>
+        <translation>Capture</translation>
+    </message>
+    <message>
+        <location filename="../SettingsEditor.ui" line="45"/>
+        <source>Скопировать</source>
+        <translation>Copy</translation>
+    </message>
+    <message>
+        <location filename="../SettingsEditor.ui" line="61"/>
+        <source>Повторить</source>
+        <translation>Repeat</translation>
+    </message>
+    <message>
+        <location filename="../SettingsEditor.ui" line="77"/>
+        <source>Распознавание</source>
+        <translation>Recognition</translation>
+    </message>
+    <message>
+        <location filename="../SettingsEditor.ui" line="83"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Необходимо для распознавания.&lt;/p&gt;&lt;p&gt;Скачивается отсюда: &lt;a href=&quot;https://code.google.com/p/tesseract-ocr/downloads/list&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;https://code.google.com/p/tesseract-ocr/downloads/list&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Required for recognition.&lt;/p&gt;&lt;p&gt;Can me downloaded from there: &lt;a href=&quot;https://code.google.com/p/tesseract-ocr/downloads/list&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;https://code.google.com/p/tesseract-ocr/downloads/list&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../SettingsEditor.ui" line="86"/>
+        <location filename="../SettingsEditor.cpp" line="72"/>
+        <source>Путь к tessdata</source>
+        <translation>Tessdata path</translation>
+    </message>
+    <message>
+        <location filename="../SettingsEditor.ui" line="99"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../SettingsEditor.ui" line="106"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Заполняется на основании содержания tessdata&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Values depend on tessdata contents&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../SettingsEditor.ui" line="109"/>
+        <source>Язык распознавания</source>
+        <translation>Recognition language</translation>
+    </message>
+    <message>
+        <location filename="../SettingsEditor.ui" line="119"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Масштабирование изображения для улучшения распознания. Больше - лучше (до определенных пределов), но медленнее.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Image scaling for better recognition. More is better (reasonable), but slower.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../SettingsEditor.ui" line="122"/>
+        <source>Увеличение масштаба</source>
+        <translation>Scale factor</translation>
+    </message>
+    <message>
+        <location filename="../SettingsEditor.ui" line="154"/>
+        <source>Вывод результата</source>
+        <translation>Result representation</translation>
+    </message>
+    <message>
+        <location filename="../SettingsEditor.ui" line="160"/>
+        <source>Трей</source>
+        <translation>Tray</translation>
+    </message>
+    <message>
+        <location filename="../SettingsEditor.ui" line="167"/>
+        <source>Окно</source>
+        <translation>Window</translation>
+    </message>
+    <message>
+        <location filename="../SettingsEditor.ui" line="180"/>
+        <source>Перевод</source>
+        <translation>Translation</translation>
+    </message>
+    <message>
+        <location filename="../SettingsEditor.ui" line="186"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Язык, на который осуществляется перевод.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Translated text language.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../SettingsEditor.ui" line="189"/>
+        <source>Язык результата</source>
+        <translation>Result language</translation>
+    </message>
+</context>
+<context>
+    <name>Translator</name>
+    <message>
+        <location filename="../Translator.cpp" line="43"/>
+        <source>Неверные парметры для перевода.</source>
+        <translation>Wrong translation parameters.</translation>
+    </message>
+    <message>
+        <location filename="../Translator.cpp" line="58"/>
+        <source>Ошибка перевода: %1</source>
+        <translation>Translation error: %1</translation>
+    </message>
+    <message>
+        <location filename="../Translator.cpp" line="73"/>
+        <source>Ошибка разбора перевода: %1 (%2)</source>
+        <translation>Translation parsing error: %1 (%2)</translation>
+    </message>
+</context>
+</TS>
