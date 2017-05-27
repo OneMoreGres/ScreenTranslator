a84e2fa72ab085e70002210a2a96f2f8e2ca9c17
Gres <gres@gres.biz>
2017-05-27 12:33:43 +0300

    Updated translators.

--- translators/yandex.js
+++ translators/yandex.js
@@ -6,14 +6,19 @@ function checkFinished () {
     if (!isPageLoaded || !isTranslationFinished || isScheduled) return;
     isScheduled = true;
     setTimeout(function () {
-        var spans = [].slice.call (document.querySelectorAll ('#translation > span'));
+        var spans = [].slice.call (document.querySelectorAll ('span.translation-chunk'));
         var text = spans.reduce (function (res, i) {
-            return res + i.innerText;
+            return res + i.innerText + ' ';
         }, '');
         st_wtp.translated (text);
+        isTranslationFinished = isScheduled = false;
     }, 2000); // wait for gui fill
 }
 function onResourceLoad (url) {
+    if (url.indexOf ('/tr.json/translate?') > -1) {
+        isTranslationFinished = true;
+        checkFinished ();
+    }
 }
 st_wtp.resourceLoaded.connect (onResourceLoad);
 function onPageLoad () {
@@ -23,9 +28,8 @@ function onPageLoad () {
 window.onload = onPageLoad();
 
 function translate (){
-    var url = 'https://translate.yandex.ru/?text=' + st_wtp.sourceText + '&lang=auto-' +
-            st_wtp.resultLanguage;
+    var url = 'https://translate.yandex.ru/&lang=' + st_wtp.sourceLanguage + '-' +
+            st_wtp.resultLanguage + '?text=' + st_wtp.sourceText ;
     url = url.replace(new RegExp(' ','g') , '%20')
-    console.log(encodeURI(url));
     window.location = (url);
 }
