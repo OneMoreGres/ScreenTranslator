d55ccd82b9a2fbcd10560df294b883c3834fc5f8
Gres <gres@gres.biz>
2020-03-27 21:24:50 +0300

    Compact capture editor

--- src/capture/captureareaeditor.cpp
+++ src/capture/captureareaeditor.cpp
@@ -17,7 +17,7 @@
 CaptureAreaEditor::CaptureAreaEditor(CaptureAreaSelector &selector)
   : QWidget(&selector)
   , selector_(selector)
-  , doTranslation_(new QCheckBox(tr("Translate"), this))
+  , doTranslation_(new QCheckBox(tr("Translate:"), this))
   , sourceLanguage_(new QComboBox(this))
   , targetLanguage_(new QComboBox(this))
   , sourceLanguageModel_(std::make_unique<QStringListModel>())
@@ -27,16 +27,13 @@ CaptureAreaEditor::CaptureAreaEditor(CaptureAreaSelector &selector)
 
   auto layout = new QGridLayout(this);
   auto row = 0;
-  layout->addWidget(doTranslation_, row, 0, 1, 2);
-
-  ++row;
-  layout->addWidget(new QLabel(tr("Recognition language:")), row, 0);
+  layout->addWidget(new QLabel(tr("Recognize:")), row, 0);
   layout->addWidget(sourceLanguage_, row, 1);
   auto swapLanguages = new QPushButton(tr("⇵"));
   layout->addWidget(swapLanguages, row, 2, 2, 1);
 
   ++row;
-  layout->addWidget(new QLabel(tr("Translation language:")), row, 0);
+  layout->addWidget(doTranslation_, row, 0);
   layout->addWidget(targetLanguage_, row, 1);
 
   sourceLanguage_->setModel(sourceLanguageModel_.get());
